ac7fb81043e822bf9a70b930dbba4cfbf4912940
Rob Erskine <rob@roberskine.com>
2022-07-29 13:58:28 -0400

    adding in automatic deploys in netlify serverless functions

---
+++ functions/deploy.js
@@ -0,0 +1,22 @@
+const handler = async function(event, context) {
+    console.log("Received event:", event)
+
+    var exec = require('child_process').exec;
+
+    var cmd = exec("npm run pages && npm run generate && npm run socialImages", function(err, stdout, stderr) {
+    if (err) {
+        // handle error
+    }
+    console.log(stdout);
+    });
+
+    dir.on('exit', function (code) {
+        // return value from "npm build"
+    });
+
+    return {
+        statusCode: 200,
+    };
+};
+
+module.exports.handler = handler;
